a2bc63fbe41ae5e9b88215004dd6871292b9db95
Billy Pak <pak.billy@gmail.com>
2020-10-29 01:12:37 -0700

    test commit

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -149,7 +149,7 @@ class FollowerListVC: UIViewController {
             // magic function for animation
             self.dataSource.apply(snapshot, animatingDifferences: true)
         }
-        // random comment here ignore
+        // random comment here ignore that is already here
     }
     
     @objc func addButtonTapped() {
